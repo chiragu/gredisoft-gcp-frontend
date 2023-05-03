1b00f2639c9b92ce37e92d261d7fd5ce89dffb4b
chiragu <chiragu@users.noreply.github.com>
2023-05-03 08:58:47 -0400

    M3D Added

--- src/pages/M3D.js
+++ src/pages/M3D.js
@@ -1,12 +1,47 @@
-import { Link } from "react-router-dom";
+import { Canvas, useLoader } from "@react-three/fiber";
+import {OrbitControls, Stars} from "@react-three/drei";
+import * as THREE from "three";
+import moonimg from "../images/moon.jpg";
+import earthimg from "../images/earth.jpg";
+
+
+const Moon = () => {
+    const texture = useLoader(THREE.TextureLoader, moonimg)
+    return (
+      <mesh position={[0,0,0]}>
+        <sphereBufferGeometry attach="geometry" />
+        <meshLambertMaterial attach="material" color="white" map={texture} />
+      </mesh>
+    );
+}
+
+const Earth = () => {
+    const texture = useLoader(THREE.TextureLoader, earthimg)
+    return (
+      <mesh position={[-35,20,-80]} scale={[3.7,3.7,3.7]}>
+        <sphereBufferGeometry attach="geometry" />
+        <meshLambertMaterial attach="material" color="white" map={texture} />
+      </mesh>
+    );
+}
+
+
 
 const M3D = () => {
     return ( 
             <div className="card my-card">
                 <div className="card-header my-card-header">Moon 3D</div>
                 <div className="card-body my-card-body">
-                    <p>Coming soon...<br/><br/>This app is under construction. Please check back later for updates.</p>
-                    <Link to="/">&lt; Back to homepage</Link>
+                    <div className="m3d-canvas">
+                        <Canvas>
+                            <OrbitControls />
+                            <Stars />
+                            <ambientLight intensity={0.5} />
+                            <spotLight position={[10, 15, 10]} angle={0.3} />
+                            <Moon />
+                            <Earth />                        
+                        </Canvas>
+                    </div>
                 </div>
             </div>           
 
